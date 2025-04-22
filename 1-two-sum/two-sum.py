83e77386f499b185a1f48dfd1224f655af63a9aa
Mh Priya <121090432+MhPriya@users.noreply.github.com>
2025-04-22 23:31:15 +0530

    Time: 1 ms (58.75%) | Memory: 18.9 MB (51.88%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -18,10 +18,10 @@ class Solution:
     # # If no solution is found, return an empty list
     #     return []
 
-        num_2_index = {}
+        num_to_index = {}
         for index, num in enumerate(nums):
             complement = target - num
-            if complement in num_2_index:
-                return(num_2_index[complement], index)
-            num_2_index[num]= index
+            if complement in num_to_index:
+                return(num_to_index[complement], index)
+            num_to_index[num]= index
         return[]
